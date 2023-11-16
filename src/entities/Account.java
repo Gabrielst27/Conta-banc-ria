5174907a343227074b0dfac98d23a24773c01915
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-15 22:51:07 -0300

    Construída Account

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -1,5 +1,39 @@
 package entities;
 
-public class Account {
+public abstract class Account {
+	
+	protected Integer number;
+	protected String holder;
+	protected Double balance;
+	protected Double withdrawLimit;
+	
+	public Account() {
+	}
+
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+	
+	public abstract void deposit(Double amount);
+	public abstract void withdraw(Double amount);
 
 }
