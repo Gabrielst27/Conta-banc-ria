3317e5cc897a1dd7b7ebaa67611896375412be51
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-17 15:58:53 -0300

    Update Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,29 +18,40 @@ public class Program {
 		
 		System.out.println("Enter account data");
 		
-		createAccount();
-		
 		try {
+			int aux = 1;
+			String type = "";
+			while(aux == 1) {
+				System.out.print("Type (c/b/s): ");
+				type = sc.nextLine();
+				
+				
+				if(type.toUpperCase().equals("C") || type.toUpperCase().equals("B") || type.toUpperCase().equals("S")) {
+					aux = 0;
+				} else {
+					System.out.println("Invalid value! Try again.");
+				}
+			}
 			System.out.print("Number: ");
 			int number = sc.nextInt();
 			sc.nextLine();
 			System.out.print("Holder: ");
 			String holder = sc.nextLine();
-			System.out.print("Initital balance: ");
+			System.out.print("Initial balance: ");
 			double balance = sc.nextDouble();
 			System.out.print("Withdraw Limit: ");
 			double withdrawLimit = sc.nextDouble();
 			
-			if(createAccount().toUpperCase().equals("C")) {
+			if(type.toUpperCase().equals("C")) {
 				account = new Account(number, holder, balance, withdrawLimit);
 				
-			} else if(createAccount().toUpperCase().equals("B")) {
+			} else if(type.toUpperCase().equals("B")) {
 				System.out.print("Loan limit: ");
 				double loanLimit = sc.nextDouble();
 				
 				account = new BusinessAccount(number, holder, balance, withdrawLimit, loanLimit);		
 				
-			} else if(createAccount().toUpperCase().equals("S")) {
+			} else if(type.toUpperCase().equals("S")) {
 				System.out.print("Interest rate: ");
 				double interestRate = sc.nextDouble();
 				
@@ -58,14 +69,14 @@ public class Program {
 			account.withdraw(sc.nextDouble());
 			System.out.printf("Updated balance: $ %.2f", account.getBalance());
 			
-			if(createAccount().toUpperCase().equals("B")) {
+			if(type.toUpperCase().equals("B")) {
 				BusinessAccount businessAccount = (BusinessAccount)account;
 				//Realizado downcasting de Account para BusinessAccount
 				System.out.printf("\nEnter amount for loan: ");
 				businessAccount.loan(sc.nextDouble());
 				System.out.printf("Updated balance: $ %.2f", account.getBalance());
 			}
-			if(createAccount().toUpperCase().equals("S")) {
+			if(type.toUpperCase().equals("S")) {
 				SavingsAccount savingsAccount = (SavingsAccount)account;
 				System.out.printf("\nEnter the number of months to see the total income: ");
 				int months = sc.nextInt();
@@ -73,32 +84,16 @@ public class Program {
 						months, savingsAccount.updateBalance(months));
 			}
 		
+			
 		}
 		catch (InputMismatchException e) {
-			System.out.println("Error in input: invalid input type");
-		} catch (DomainException e) {
+			System.out.printf("\nError in input: invalid input type.\n\n(Try using '.' instead of ',') ");
+		}
+		catch (DomainException e) {
 			System.out.println(e.getMessage());
 		}
 		
 		sc.close();
-			
-		}
-	
-	public static String createAccount() {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Type (c/b/s): ");
-		String type = sc.nextLine();
-		
-		if(!type.toUpperCase().equals("C") && !type.toUpperCase().equals("B") && !type.toUpperCase().equals("S")) {
-			System.out.printf("Invalid value! Try again.\n\n");
-			createAccount();
-		}
-			
-		sc.close();
-		
-		return type;
 		
 	}
 
